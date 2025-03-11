7e6115126323c1844155920583f34e912380d9b2
Samuelz <al@samuelz.com>
2025-03-11 11:35:10 +0100

    Build the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const IssuesPage = () => {
-  return <div><Button>New Issue</Button></div>;
+  return (
+    <div>
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
