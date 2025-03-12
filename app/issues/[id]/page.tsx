025fe0559686e30e0a505dc15f50bb6f551007a8
Samuelz <al@samuelz.com>
2025-03-12 09:47:35 +0100

    Show issue details

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,23 @@
+import { prisma } from "@/prisma/client";
+import { notFound } from "next/navigation";
+
+const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!issue) notFound();
+
+  return (
+    <div>
+      <p>{issue.title}</p>
+      <p>{issue.description}</p>
+      <p>{issue.status}</p>
+      <p>{issue.createdAt.toDateString()}</p>
+    </div>
+  );
+};
+
+export default IssueDetailPage;
